9309d64a1d824be62e733b3b4481d843c32594df
Joseph Pilon <jap2739@.rit.edu>
2024-09-03 00:16:51 -0400

    Addeda few components to get the basics for the nav bar for the website. Just made sure that I was able to get the pages to change based on the button clicked.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about-us', component:AboutUsComponent},
+  {path: 'schedule', component:ScheduleComponent},
+  {path: '',redirectTo: '/about-us', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ScheduleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
