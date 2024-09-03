ab39b7296f50a8aa9aace440924016b2047deab4
Joseph Pilon <jap2739@.rit.edu>
2024-09-03 16:38:52 -0400

    Made a nav bar at the top of the page that will allow the user to navigate to different pages of the website. Each page has will have its own contents and it already changes between pages if pressed by the user.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ScheduleComponent } from './schedule/schedule.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'about-us', component:AboutUsComponent},
   {path: 'schedule', component:ScheduleComponent},
-  {path: '',redirectTo: '/about-us', pathMatch:'full'}
+  {path: 'home', component:HomeComponent},
+  {path: 'reviews', component:ReviewsComponent},
+  {path: '',redirectTo: '', pathMatch:'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { RouterModule } from '@angular/router';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
     ScheduleComponent,
+    ReviewsComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
